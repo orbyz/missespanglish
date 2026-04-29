8ccadabf13ef005d248c72fdcd12ba8aa47f0029
Jonathan Olbes <olbes.es@gmail.com>
2026-04-29 08:48:23 +0200

    fix: Link to Candely session

--- src/lib/constants/contact.ts
+++ src/lib/constants/contact.ts
@@ -1,6 +1,6 @@
 export const CONTACT = {
   whatsapp: "34633047312",
-  calendly: "https://calendly.com/missespanglish",
+  calendly: "https://calendly.com/missespanglish/clase-prueba",
   defaultMessage:
     "Hola Miss Espanglish! Me gustaría información sobre tus clases",
 };
